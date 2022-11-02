2f6dbad2b732fb8c68be7eb628bb2e1fb8d1c821
fernandobouchet <fernandobouchet@gmail.com>
2022-11-01 23:41:47 -0300

    Revert "Use 'hashRouter' instead of 'BrowserRouter' to fix reload in dynamic page routes"
    This reverts commit 07d77836ad5afb1f7db3923e31cf4fc56d8245be.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
